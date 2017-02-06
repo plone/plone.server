17fe8800b333db02486151b5b220078d6ce8f0b7
Ramon Navarro Bosch <ramon@atlasense.com>
2017-02-06 01:59:00 +0100

    More testing

--- src/plone.server/plone/server/tests/test_security.py
+++ src/plone.server/plone/server/tests/test_security.py
@@ -4,9 +4,25 @@ from plone.server.testing import PloneFunctionalTestCase
 from plone.server.tests import TEST_RESOURCES_DIR
 from zope import schema
 from zope.interface import Interface
+from plone.server.interfaces import IRequest
+from zope.interface import alsoProvides
+from plone.server.browser import View
+from aiohttp.test_utils import make_mocked_request
+from plone.server.auth import get_roles_with_access_content
+from plone.server.auth import get_principals_with_access_content
 
 import json
 import os
+
+
+class PrincipalsView(View):
+    def __call__(self):
+        return get_principals_with_access_content(self.context)
+
+
+class RolesView(View):
+    def __call__(self):
+        return get_roles_with_access_content(self.context)
 
 
 class FunctionalTestServer(PloneFunctionalTestCase):
@@ -59,3 +75,52 @@ class FunctionalTestServer(PloneFunctionalTestCase):
         self.assertTrue(response['inherit'][0]['prinrole']['root']['plone.SiteAdmin'] == 'Allow')
         self.assertTrue(response['inherit'][0]['prinrole']['root']['plone.Owner'] == 'Allow')
         self.assertTrue(response['inherit'][0]['prinperm']['user1']['plone.AccessContent'] == 'AllowSingle')
+
+        # Check the access users/roles
+        testing_object = self._get_site()['testing']
+        request = make_mocked_request('POST', '/')
+        alsoProvides(request, IRequest)
+        principals = PrincipalsView(testing_object, request)()
+        self.assertEqual(principals, ['root'])
+        request = make_mocked_request('POST', '/')
+        alsoProvides(request, IRequest)
+        roles = RolesView(testing_object, request)()
+        self.assertEqual(roles, ['plone.SiteAdmin'])
+
+        # Now we add the user1 with inherit on the site
+        resp = self.layer.requester(
+            'POST',
+            '/plone/plone/@sharing',
+            data=json.dumps({
+                'type': 'Allow',
+                'prinperm': {
+                    'user1': [
+                        'plone.AccessContent'
+                    ]
+                }
+            })
+        )
+        testing_object = self._get_site()['testing']
+        request = make_mocked_request('POST', '/')
+        alsoProvides(request, IRequest)
+        principals = PrincipalsView(testing_object, request)()
+        self.assertEqual(principals, ['user1', 'root'])
+
+        # Now we add the user1 with deny on the object
+        resp = self.layer.requester(
+            'POST',
+            '/plone/plone/testing/@sharing',
+            data=json.dumps({
+                'type': 'Deny',
+                'prinperm': {
+                    'user1': [
+                        'plone.AccessContent'
+                    ]
+                }
+            })
+        )
+        testing_object = self._get_site()['testing']
+        request = make_mocked_request('POST', '/')
+        alsoProvides(request, IRequest)
+        principals = PrincipalsView(testing_object, request)()
+        self.assertEqual(principals, ['root'])
